9727777a016b6734bbbf76ca3367eea3ee8eb5d2
imadarif@live.com <imadarif@live.com>
2015-06-20 12:31:57 -0700

    simple left sidenav working.

---
+++ bower_components/angular-material/modules/closure/whiteframe/whiteframe.js
@@ -0,0 +1,15 @@
+/*!
+ * Angular Material Design
+ * https://github.com/angular/material
+ * @license MIT
+ * v0.10.0-master-12a28aa
+ */
+goog.provide('ng.material.components.whiteframe');
+
+/**
+ * @ngdoc module
+ * @name material.components.whiteframe
+ */
+angular.module('material.components.whiteframe', []);
+
+ng.material.components.whiteframe = angular.module("material.components.whiteframe");
